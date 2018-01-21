ca8b80bc91641ea8a9dbc8e0c25dc8e752127ac7
Takayoshi <tkys0515@gmail.com>
2018-01-21 21:21:31 +0900

    Added fx:id to timeSeriesLineChart.

--- src/tech/tkys/fft/main/PrimaryRootPaneController.java
+++ src/tech/tkys/fft/main/PrimaryRootPaneController.java
@@ -2,11 +2,16 @@ package tech.tkys.fft.main;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.chart.LineChart;
 import tech.tkys.fft.test.FFTTestService;
 
 import java.util.ArrayList;
 
 public class PrimaryRootPaneController {
+
+    @FXML
+    private LineChart<Double, Double> timeSeriesLineChart;
+
     @FXML
     public void onSelectButtionClicked(ActionEvent event) {
         System.out.println("onSelectButtionClicked");
