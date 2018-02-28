20419af868e86424cb1ea906b59bfa4a424d30d9
Takayoshi <tkys0515@gmail.com>
2018-03-01 06:16:01 +0900

    Added code comments.

--- src/tech/tkys/fft/main/FFT.java
+++ src/tech/tkys/fft/main/FFT.java
@@ -75,41 +75,45 @@ public class FFT {
     }
 
     /**
-     * Prepares complex sine wave
+     * Prepares array of sine values
+     * Note: Cosine values are not necessary because they can be obtained by shifting phase (π/2) of sine values.
      * @param sampleNumber
-     * @return
+     * @return Array of sine values
      */
     private static Double[] makeSinArray(int sampleNumber) {
         Double[] sinArray = new Double[sampleNumber + sampleNumber / 4];
 
-        Double t = Math.sin(Math.PI / sampleNumber);
-        Double dc = 2 * t * t;                  // cos?
-        Double ds = Math.sqrt(dc * (2 - dc));   // sin?
+        Double t = Math.sin(Math.PI / sampleNumber); // コストのかかるライブラリ呼び出しは1回だけ
+        Double dc = 2 * t * t;                  // π/2[rad]起点の変分の初期値
+        Double ds = Math.sqrt(dc * (2 - dc));   // 0[rad]起点の変分の初期値
         t = 2 * dc;
 
-        Double cosValue = sinArray[sampleNumber / 4] = 1.0;
-        Double sinValue = sinArray[0] = 0.0;
+        Double ｃ = sinArray[sampleNumber / 4] = 1.0;    // sin(π/2) = 1
+        Double ｓ = sinArray[0] = 0.0;                   // sin(0) = 0
 
+        // 三角関数の絶対値が実質的に変化する 0 ~ π/2 の間の値を計算する。
+        // 効率のため 0 側と π/2 側の両側から計算を進行させ繰り返し数を半分にする。
         for (int i = 1; i < sampleNumber / 8; i++) {
-            cosValue -= dc;
-            dc += t * cosValue;
-            sinValue += ds;
-            ds -= t * sinValue;
-            sinArray[i] = sinValue;
-            sinArray[sampleNumber / 4 - i] = cosValue;
+            ｃ -= dc;
+            dc += t * ｃ;    // π/2[rad]起点の変分はだんだん増えていく
+            ｓ += ds;
+            ds -= t * ｓ;    // 0[rad]起点の変分はだんだん減っていく
+            sinArray[i] = ｓ;
+            sinArray[sampleNumber / 4 - i] = ｃ;
         }
 
-        //
+        // sin(π/4)
         if (sampleNumber / 8 != 0) {
             sinArray[sampleNumber / 8] = Math.sqrt(0.5);
         }
 
-        // 周期性
+        // π/2 ~ π の範囲のsin値：0 ~ π/2 のsin値をコピーする。
         for (int i = 0; i < sampleNumber / 4; i++) {
             sinArray[sampleNumber / 2 - i] = sinArray[i];
         }
 
-        // 周期性
+        // π ~ 2.5π の範囲のsin値：0 ~ π/2 のsin値を、符号を反転させてコピーする。
+        // π/2余分に用意する：sin(θ+π/2)=cos(θ)であり、FFTの計算でsinArrayからcosの値を取り出しやすくするため。
         for (int i = 0; i < sampleNumber /2 + sampleNumber / 4; i++) {
             sinArray[i + sampleNumber / 2] = - sinArray[i];
         }
